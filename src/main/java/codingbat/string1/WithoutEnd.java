4a1ed6a036a3390b4308e75c30a5cde86820d18f
Aleksandra <alex88kg@gmail.com>
2024-09-01 16:56:20 +0600

    String1/WithoutEnd

---
+++ src/main/java/codingbat/string1/WithoutEnd.java
@@ -0,0 +1,10 @@
+package codingbat.string1;
+
+public class WithoutEnd {
+    public String withoutEnd(String str) {
+
+        return str.length() >= 2 ? str.substring(1,str.length()-1) : str ;
+
+    }
+
+}
