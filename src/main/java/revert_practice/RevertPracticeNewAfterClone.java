73d98466513ced5937c02c9ee8cf914ccf6735df
Aleksandra <alex88kg@gmail.com>
2024-09-04 23:44:55 +0600

    Revert Practice New After Clone

---
+++ src/main/java/revert_practice/RevertPracticeNewAfterClone.java
@@ -0,0 +1,7 @@
+package revert_practice;
+
+public class RevertPracticeNewAfterClone {
+    public static void main(String[] args) {
+        System.out.println("Revert Practice New After Clone");
+    }
+}
